24bde411a577ba901f9061ec33cb56e34a972df4
Your Name <you@example.com>
2016-07-27 17:31:41 +0200

    unnecessary improvements

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import subprocess
 import re
 import socket
 
-filtered = ['192.168', '10']
+filtered = ['192.168', '10', '255.255.255.255']
 
+logfile = open('/var/log/access.log', 'a')
 f = subprocess.Popen(['tail','-F',"/var/log/ulog/syslogemu.log"],\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 while True:
@@ -15,17 +16,17 @@ while True:
     l = json.loads(str(line))
 
     src_ip = l['src_ip']
-    dest_ip = l['dest_ip']
-    mac = l['mac.saddr.str']
+    destination = l['dest_ip'].strip()
+    source = l['mac.saddr.str'].strip()
 
     try:
-        if not dest_ip.startswith(tuple(filtered)):
-            dest_ip = socket.gethostbyaddr(dest_ip)[0]
+        if  destination.startswith(tuple(filtered)):
+            continue
 
-        mac = users[mac]
+        source = users[source]
+        destination = socket.gethostbyaddr(destination)[0]
     except:
         None
     
-    print(mac, end=' ')
-    	
-    print(dest_ip)
+    logfile.write('%s %s\n' % (source, destination))
+    logfile.flush()
